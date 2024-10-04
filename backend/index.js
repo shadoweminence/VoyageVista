9d0a930b66c416e84faeeac2a399b0e623ccd4cc
Prashant <pokhrelprashant9@gmail.com>
2024-10-04 21:52:59 +0545

    i got tired its not using socket.io right now and i need to click on the user to see the latest message but chatting is possible

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,13 @@
 const connectToMongo = require("./db");
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
+
 const cors = require("cors");
 
 connectToMongo();
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
 
 app.use(cors());
 app.use(express.json());
@@ -17,19 +16,6 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tourpack", require("./routes/tourpack"));
 app.use("/api/messages", require("./routes/messages"));
 app.use("/api/search", require("./routes/search"));
-
-// Socket.io connection handling
-io.on("connection", (socket) => {
-  console.log("a user connected");
-
-  socket.on("sendMessage", (message) => {
-    io.emit("receiveMessage", message); // Broadcast to all connected clients
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
 
 const port = 5000;
 
